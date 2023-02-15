d760f8132b4d17ee3a2141c734e8176ff9fce558
Doug Clift <dougclift@gmail.com>
2023-02-14 17:37:31 -0700

    added filter function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const itemFilter = document.getElementById('filter')
+const clearBtn = document.getElementById('clear')
 
 function addItem(e) {
   e.preventDefault()
@@ -21,6 +23,9 @@ function addItem(e) {
 
   itemList.appendChild(li)  
 
+  // Check UI
+  checkUI()
+
   itemInput.value = ''
 }
 
@@ -40,5 +45,54 @@ function createIcon(classes) {
   return icon
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you Sure?')) {
+      e.target.parentElement.parentElement.remove()
+      checkUI()
+    }
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild)
+  }
+  checkUI()
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll('li')
+  const text = e.target.value.toLowerCase()
+
+  items.forEach(item => {
+    const itemName = item.firstChild.textContent.toLowerCase()
+    
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex'
+    } else {
+      item.style.display = 'none'
+    }
+
+  })
+  
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li')
+  if (items.length === 0) {
+    clearBtn.style.display = 'none'
+    itemFilter.style.display = 'none'
+  } else {
+    clearBtn.style.display = 'block'
+    itemFilter.style.display = 'block'
+  }
+}
+
 // Event Listners
 itemForm.addEventListener('submit', addItem)
+itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener('click', clearItems)
+itemFilter.addEventListener('input', filterItems)
+
+checkUI()
